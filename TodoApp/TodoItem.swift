30fc8df755e4ff05ecd44a89e4fc0e88f0f42e16
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2019-05-20 15:42:49 +0700

    implement viewController

--- TodoApp/TodoItem.swift
+++ TodoApp/TodoItem.swift
@@ -12,7 +12,7 @@ class TodoItem {
     var title: String
     var isDone: Bool
 
-    init(title: String, isDone: Bool) {
+    init(title: String, isDone: Bool = false) {
         self.title = title
         self.isDone = isDone
     }
--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    var todo = Todo()
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.totalItems
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        todo.add(item: TodoItem(title: "Buy milk"))
+        todo.add(item: TodoItem(title: "Learning Swift", isDone: false))
     }
-
-
 }
 
