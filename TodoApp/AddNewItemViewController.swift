6d94bdd1c6bdacb9b57bdab3442d6a8f31219ce8
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2019-05-20 21:09:24 +0700

    finish add new item without mock

--- TodoApp/AddNewItemViewController.swift
+++ TodoApp/AddNewItemViewController.swift
@@ -17,12 +17,18 @@ class AddNewItemViewController: UIViewController {
 
     weak var delegate: AddNewItemViewControllerDelegate?
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var isDoneSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func doneButtonDidTap(_ sender: UIBarButtonItem) {
-        delegate?.addNewItemViewController(controller: self, didAdd: TodoItem(title: "Test"))
+        if let title = titleTextField.text, !title.isEmpty {
+            let todoItem = TodoItem(title: title, isDone: isDoneSwitch.isOn)
+            delegate?.addNewItemViewController(controller: self, didAdd: todoItem)
+        }
     }
 
     @IBAction func cancelButtonDidTap(_ sender: UIBarButtonItem) {
